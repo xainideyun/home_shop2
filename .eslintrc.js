7044a91305b4e85c25c3a6d1f9787f80d9d21c78
xainideyun <sunxsaljj@163.com>
2020-02-03 08:30:13 +0800

    初始化

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 0  // 引用方法名前需要加空格的校验
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
